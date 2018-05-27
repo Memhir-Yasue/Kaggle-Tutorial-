02da7562801d4733d12a0d8987c55206b4eadaa4
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-05-27 10:33:15 -0400

    Sckikit-Learn Model

--- first_model.py
+++ first_model.py
@@ -12,7 +12,8 @@ data = pd.read_csv(main_file_path)
 # Summary of data  
 print(data.describe())
 
-#---------------------------------------
+# *********************************
+# ********************************
 
 
 # Selecting and Filtering Data
@@ -35,3 +36,41 @@ print(two_columns_of_data.head())
 #describing data
 two_columns_summary = two_columns_of_data.describe()
 print(two_columns_summary)
+
+# ********************************************
+# ********************************************
+
+# Building model #
+
+# Prediction target/ column we want to predict
+y = data.SalePrice
+
+# loading predictors
+#fireplace, fullbath, yearbuilt
+
+data_predictors = ['YearBuilt','FullBath','Fireplaces']
+
+X = data[data_predictors]
+
+
+# Define: What type of model will it be? A decision tree? Some other type of model? Some other parameters of the model type are specified too.
+# Fit: Capture patterns from provided data. This is the heart of modeling.
+# Predict: Just what it sounds like
+# Evaluate: Determine how accurate the model's predictions are.
+
+from sklearn.tree import DecisionTreeRegressor 
+
+# Define model
+data_model = DecisionTreeRegressor()
+
+# Fit model
+data_model.fit(X,y)
+
+# parameters aboyt the type of model built
+# print(data_model.fit(X,y))
+print('\n')
+print("Making predictions for the first 5 houses")
+print(X.head(),'\n')
+print("The predictions are")
+print(data_model.predict(X.head()))
+
